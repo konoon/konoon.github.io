38938ad317f442e3767c0c4a2004824ed804b8ca
fondl <fondl@LAPTOP-SR0BJ691>
2023-05-09 12:21:24 +0900

    Scanner 예제

---
+++ JAVA_FIRST/src/study/exam01.java
@@ -0,0 +1,15 @@
+package study;
+
+import java.util.Scanner;
+
+public class exam01 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+		int A = scan.nextInt();
+		int B = scan.nextInt();
+		
+		System.out.println(A + B);
+	}
+}
