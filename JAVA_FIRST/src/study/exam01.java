c8900125747e4210427e18cbc863a8307712795c
fondl <fondl@LAPTOP-SR0BJ691>
2023-05-10 13:30:06 +0900

    문자무시 예시 커밋

--- JAVA_FIRST/src/study/exam01.java
+++ JAVA_FIRST/src/study/exam01.java
@@ -7,9 +7,11 @@ public class exam01 {
 		Scanner scan = new Scanner(System.in);
 		
 		// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
-		int A = scan.nextInt();
-		int B = scan.nextInt();
+		//int height = scan.nextInt();
+		//String result = height >= 120 ? "가능" : "불가능";
 		
-		System.out.println(A + B);
+		//System.out.println("키가 " + height + "cm 이므로 탑승 " + result + "합니다.");
+		String s = "Java";
+		System.out.println(s.equalsIgnoreCase("java"));
 	}
 }
